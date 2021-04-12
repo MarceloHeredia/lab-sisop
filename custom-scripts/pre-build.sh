22458fff037dd9df3bc715749540dcafa6151037
MarceloHeredia <heredia_m@outlook.com>
2021-04-12 04:27:42 -0300

    custom script start_server working

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -2,3 +2,6 @@
   
   cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
   chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+  cp $BASE_DIR/../custom-scripts/*.py $BASE_DIR/target/usr/bin
+  cp $BASE_DIR/../custom-scripts/start_server $BASE_DIR/target/usr/bin
+  chmod +x $BASE_DIR/target/usr/bin/start_server
